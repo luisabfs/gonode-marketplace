46dd4e0449bfec0c60b84c29e21e4bc5b9a9e9f0
Luísa Barros <qlullyq@gmail.com>
2019-05-06 22:28:05 -0300

    User scheme

--- src/routes.js
+++ src/routes.js
@@ -2,4 +2,8 @@ const express = require('express');
 
 const routes = express.Router();
 
+const UserController = require('./app/controllers/UserController');
+
+routes.post('/users', UserController);
+
 module.exports = routes;
